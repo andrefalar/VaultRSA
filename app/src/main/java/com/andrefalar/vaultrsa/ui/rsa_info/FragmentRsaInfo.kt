25bde61854957c5fad94eed4787aa25a5725e9c9
Andrés Alarcón <andre.alarcon@proton.me>
2024-07-17 14:04:40 -0500

    bottom menu initial configuration, and FragmentRsaInfo added

---
+++ app/src/main/java/com/andrefalar/vaultrsa/ui/rsa_info/FragmentRsaInfo.kt
@@ -0,0 +1,25 @@
+package com.andrefalar.vaultrsa.ui.rsa_info
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.andrefalar.vaultrsa.R
+import com.andrefalar.vaultrsa.databinding.FragmentRsaInfoBinding
+
+class FragmentRsaInfo : Fragment() {
+
+    private var _binding: FragmentRsaInfoBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRsaInfoBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
