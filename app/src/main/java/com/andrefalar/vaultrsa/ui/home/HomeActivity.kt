c870db5edf7171de3fc902be98fac22e848a85f7
Andrés Alarcón <andre.alarcon@proton.me>
2024-07-21 21:35:59 -0500

    provisional Logout bottom added

--- app/src/main/java/com/andrefalar/vaultrsa/ui/home/HomeActivity.kt
+++ app/src/main/java/com/andrefalar/vaultrsa/ui/home/HomeActivity.kt
@@ -2,11 +2,14 @@ package com.andrefalar.vaultrsa.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.andrefalar.vaultrsa.R
 import com.andrefalar.vaultrsa.databinding.ActivityHomeBinding
+import com.google.firebase.auth.FirebaseAuth
 
 enum class ProviderType {
     BASIC
@@ -25,24 +28,47 @@ class HomeActivity : AppCompatActivity() {
         initUI()
     }
 
+
     private fun initUI() {
         initNavigation()
+        initToolbar()
+    }
 
+    private fun initToolbar() {
+        setSupportActionBar(binding.toolbar)
+        // Delete the Toolbar default title
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        // Change Toolbar title according the fragment
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.toolbarTitle.text = destination.label
+        }
     }
 
     private fun initNavigation() {
-        setSupportActionBar(binding.toolbar)
-
-        // Delete the Toolbar default title
-        supportActionBar?.setDisplayShowTitleEnabled(false)
 
         val navHost =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         navController = navHost.navController
         binding.bottomNavigation.setupWithNavController(navController)
+    }
 
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            binding.toolbarTitle.text = destination.label
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_logout -> {
+                logout()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun logout() {
+        FirebaseAuth.getInstance().signOut()
+        onBackPressedDispatcher.onBackPressed()
     }
 }
